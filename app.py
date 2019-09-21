4feb573217be9399c4cde293a6943ffd49ad90f6
Anamaria Ardila <aa4278@columbia.edu>
2019-09-21 12:01:38 -0400

    Added a function that displays the name of the building typed in the URL.

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
+import requests
+from flask import escape
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def main():
-    return render_template('index.html')
-
+	#return render_template('index.html')
+	show_building('butler')
+	
+@app.route('/information/<building_name>')
+def show_building(building_name):
+	return 'Building is: %s' % escape(building_name)
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
