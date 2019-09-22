2e91a7ee328f2a7f40ecaa6e310fc4ddb8b56736
Anamaria Ardila <aa4278@columbia.edu>
2019-09-21 20:26:41 -0400

    show_number now shows the n-th least filled libraries in order.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import requests
 from flask import escape
 import json
+
 from ast import literal_eval
 app = Flask(__name__)
 
@@ -29,7 +30,7 @@ def decide_query_type(query):
 	if is_name == True:
 		return show_building(parent_id)
 	else:
-		return show_number(query)
+		return show_number(int(query))
 
 
 def show_building(parent_id):
@@ -52,7 +53,22 @@ def show_building(parent_id):
 
 def show_number(num_groups):
 
-	return '%s' % escape(num_groups)
+	key = "-VsZgGinNEOP9UZyeYhElGo6EpdmeMQq6lpJRfZYhO4vh0SH56NLQo54p0oh7Qh_"
+	auth = {'auth_token' : key}
+	r = requests.get('https://density.adicu.com/latest', params=auth)
+	data = json.loads(r.text)
+	list_of_dicts = data.get('data')
+	list_of_strings = []
+	new_list = sorted(list_of_dicts, key = lambda i: i['percent_full'])
+	
+	key1 = 'group_name'
+	key2 = 'percent_full'
+	for x in range(num_groups):
+		dict = new_list[x]
+		line = dict.get(key1) + ' is ' + str(dict.get(key2)) + '% full'
+		list_of_strings.append(line)		
+
+	return json.dumps(list_of_strings)
 
 
 
